7a173bc12fe38340c5d648269ec3f3d347b308a0
Ian Black <blackia@oregonstate.edu>
2020-05-30 13:23:32 -0700

    Updated examples.

--- examples/example_ec.py
+++ examples/example_ec.py
@@ -1,10 +1,8 @@
 '''Description
-Example script that showcases AtlasEC class functions.
+Example script that showcases commonly used AtlasEC class functions.
 '''
 
 import datetime
-import fcntl
-import io
 import pandas as pd
 import time
 from atlas import AtlasEC
@@ -13,26 +11,65 @@ from atlas import AtlasEC
 
 def main(): 
     ec = AtlasEC() #Instantiate the class.
+    
+    
+    #Get device information.
     device,firmware = ec.info()  #Get EZO type and firmware version.
+    reason,voltage = ec.status() #Get the reason for restart and voltage at VCC
+    
+    
+    #Set and get device name.
+    ec.set_device_name('PiCTD')
+    name = ec.get_device_name()
+    print(name)
     
     #Showcase LED functions
+    ec.led_on() 
+    ec.led_find(5)  #Test out the EZO find function for 10 seconds.
     ec.led_off()
-    led_status = ec.led_status()
+    ec.led_status()  #Make sure the LED is off.
+    time.sleep(3)
+    print('Turning LED on.')
     ec.led_on()
+    ec.led_status()
     
-    #Change output when taking a sample.
-    output = ec.output(['EC'])
     
-    data = []
+    #Set and check the temperature compensation setting.
+    ec.set_temp_comp(0.00)
+    comp = ec.get_temp_comp()
+    print(comp)
+    
+    
+    ec.set_temp_comp(25.00)
+    comp = ec.get_temp_comp()
+    print(comp)
+    
+    
+    #Change output of a sample.
+    output = ec.output(['EC','TDS','S','SG'])
+    print(output)
+    
+
+    #Take a sample once per second for 10 seconds and put it in a data frame. 
+    #Time between samples is 0.4 s since it takes ~0.6 s to take a sample.
+    data = pd.DataFrame()
     for i in range(10):
         now = datetime.datetime.utcnow()
-        sample = ec.take_sample() #Take a reading to confirm that there is only one value output.
-        d = pd.DataFrame(data = {'datetime':[now],'ec':[sample]})
-        data = pd.concat([data,d])
-        
-        
-        data.append(sample)
-        time.sleep(1)
+        sample = ec.take_sample() #Take a reading.
+        EC = sample[0]  #Split the array up.
+        TDS = sample[1]
+        S = sample[2]
+        SG = sample[3]
+        d = pd.DataFrame(data = {'datetime':[now],
+                                 'EC':[EC],
+                                 'TDS':[TDS],
+                                 'S':[S],
+                                 'SG':[SG]})
+        data = pd.concat([data,d])  #Concatenate with previous for loop.
+        time.sleep(0.4)
+    data = data.reset_index(drop=True)
+    
+    print(data)
 
 
 if __name__ == "__main__":
--- examples/example_ec_cal.py
+++ examples/example_ec_cal.py
@@ -3,9 +3,6 @@ Performs a two point calibration.
 Waits for user input before moving on to the next step.
 '''
 
-
-import fcntl
-import io
 import os
 import time
 from atlas import AtlasEC
@@ -13,36 +10,37 @@ from atlas import AtlasEC
 def main():
     ec = AtlasEC() #Create the EC EZO object.
     
+    ptype = input('Please enter probe type value (i.e 0.1, 1.0, 10.0).\n')
+    ec.probe_type(ptype)
+
     print('Clearing previous calibration info.')
     ec.cal_clear()
     time.sleep(3)
     
-    r1 = input("Please dry the probe for a dry calibration. Once dry, hit enter.")
+    r1 = input("Please dry the probe for a dry calibration. Once dry, hit enter.\n")
     if r1 == '':
         ec.cal_dry()
         time.sleep(3)
     else:
-        print('Dry calibration not performed. Exit script.')
+        print('Dry calibration not performed. Exit script.\n')
         os.exit()
     
-    print('Place the probe in a low value solution.')
+    print('Place the probe in a low value solution.\n')
     time.sleep(5)
     
-    r2 = input("Please select low value for calibration")
+    r2 = input("Please select low value for calibration.\n")
     ec.cal_low(float(r2))
     time.sleep(1)
     
-    print("Place the probe in a high value solution.")
+    print("Place the probe in a high value solution.\n")
     time.sleep(5)
     
-    r3 = input("Please select high value for calibration.")
+    r3 = input("Please select high value for calibration.\n")
     ec.cal_high(float(r3))
     time.sleep(1)
     
-    status = ec.cal_status()
-    print("Cal Status: {}".format(status))
-    
-    
+    ec.cal_status()
+
     
 if __name__ == "__main__":
     main()
